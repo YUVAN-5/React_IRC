181f526b208aec5482593343c4185ec982cd06c7
YUVAN SHANKAR <yuvanganesan4325@gmail.com>
2023-12-13 15:34:37 +0530

    done

--- Day3/myapp/src/App.js
+++ Day3/myapp/src/App.js
@@ -3,7 +3,7 @@ import Footer from './components/Footer.js';
 import './Assests/css/Nav.css';
 import './Assests/css/categories.css';
 import HalHome from './components/Halhome.js';
-import Shop from '../components/Shop.js';
+import Shop from './components/Shop.js';
 
 function App() {
   return (
